2861e5ffcc73db2beac5701ab6f31852836d6418
justsixthree <bhupinder@anattadesign.com>
2022-05-25 18:27:10 +0530

    new updations

--- js/script.js
+++ js/script.js
@@ -1,2 +1,59 @@
-import header from "../js/components/header";
-header()
+let header = Vue.createApp({
+
+  data : function () {
+    return {
+      scrollPosition: null,
+      logo : '/img/moiraspace-logo.png',
+      logoAlt: 'Moiraspace',
+      navigation : {
+        left: [
+          {
+            name : 'Logos',
+            url : '/about',
+          },
+          {
+            name : 'Graphic design',
+            url : '/art',
+          },
+          {
+            name : 'Game design',
+            url : '/art',
+          }
+        ],
+        
+        right : [
+          {
+            name : 'Fine art',
+            url : '/art',
+          },
+          {
+            name : 'Community work',
+            url : '/about',
+          },
+        ],
+        
+      } 
+    }
+  },
+  
+  mounted() {
+    window.addEventListener('scroll', this.updateScroll);
+  },
+
+  created() {
+  
+  },
+
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY
+    }
+  },
+
+  computed: {
+    
+  }
+
+})
+
+header.mount('#header')
